05d3effa3b9d148c3e63a275df75643c02f76219
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-05-04 00:46:11 -0600

    Ultimo commit

--- jwt/procesoToken.go
+++ jwt/procesoToken.go
@@ -3,6 +3,7 @@ package jwt
 import (
 	"errors"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/ngonzalezo/twitterGo/bd"
 	"github.com/ngonzalezo/twitterGo/models"
 	"strings"
 )
@@ -25,6 +26,12 @@ func ProcesoToken(tk string, JWTSign string) (*models.Claim, bool, string, error
 	})
 	if err != nil {
 		//Rutina que chequea contra la base de datos
+		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
+		if encontrado {
+			Email = claims.Email
+			IDUsuario = claims.ID.Hex()
+		}
+		return &claims, encontrado, IDUsuario, nil
 	}
 	if !tkn.Valid {
 		return &claims, false, string(""), errors.New("Token invalido")
