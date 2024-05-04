981d4289df65b489e2f56eb00a555c1a8958feb5
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-05-04 01:56:15 -0600

    Ultimo commit

--- models/sm.go
+++ models/sm.go
@@ -4,6 +4,6 @@ type Secret struct {
 	Host     string `json:"host"`
 	Username string `json:"username"`
 	Password string `json:"password"`
-	JWTSign  string `json:"jwtsign"`
+	JWTSign  string `json:"jwtSign"`
 	Database string `json:"database"`
 }
