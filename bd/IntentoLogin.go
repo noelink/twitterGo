bb4004595d541177021b9cbdbfa2eb27db7d103a
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-05-04 00:29:50 -0600

    Ultimo commit

--- bd/IntentoLogin.go
+++ bd/IntentoLogin.go
@@ -1,11 +1,13 @@
 package bd
 
 import (
+	"fmt"
 	"github.com/ngonzalezo/twitterGo/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func IntentoLogin(email string, password string) (models.Usuario, bool) {
+	fmt.Println("Ingresamos a intento login")
 	usu, encontrado, _ := ChequeoYaExisteUsuario(email)
 
 	if !encontrado {
@@ -18,6 +20,7 @@ func IntentoLogin(email string, password string) (models.Usuario, bool) {
 	err := bcrypt.CompareHashAndPassword(passwordBD, passwordBytes)
 
 	if err != nil {
+		fmt.Println("VLV ocurrio un error", err.Error())
 		return usu, false
 	}
 
