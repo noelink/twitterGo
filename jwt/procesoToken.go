438106ebd6906d0aec6633387e6835ac2e0454d8
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-05-08 22:41:21 -0600

    Ultimo commit

--- jwt/procesoToken.go
+++ jwt/procesoToken.go
@@ -13,19 +13,20 @@ var IDUsuario string
 
 func ProcesoToken(tk string, JWTSign string) (*models.Claim, bool, string, error) {
 	miClave := []byte(JWTSign)
-
 	var claims models.Claim
 
 	splitToken := strings.Split(tk, "Bearer")
 	if len(splitToken) != 2 {
-		return &claims, false, string(""), errors.New("Formato de token invalido")
+		return &claims, false, string(""), errors.New("formato de token inválido")
 	}
+
 	tk = strings.TrimSpace(splitToken[1])
+
 	tkn, err := jwt.ParseWithClaims(tk, &claims, func(token *jwt.Token) (interface{}, error) {
 		return miClave, nil
 	})
-	if err != nil {
-		//Rutina que chequea contra la base de datos
+	if err == nil {
+		// Rutina que chequea contra la BD
 		_, encontrado, _ := bd.ChequeoYaExisteUsuario(claims.Email)
 		if encontrado {
 			Email = claims.Email
@@ -33,8 +34,10 @@ func ProcesoToken(tk string, JWTSign string) (*models.Claim, bool, string, error
 		}
 		return &claims, encontrado, IDUsuario, nil
 	}
+
 	if !tkn.Valid {
-		return &claims, false, string(""), errors.New("Token invalido")
+		return &claims, false, string(""), errors.New("token inválido")
 	}
+
 	return &claims, false, string(""), err
 }
