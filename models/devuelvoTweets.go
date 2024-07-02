04504a44ccfadf9657c70375919ace20cd9b02af
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-07-02 16:30:38 -0600

    Ultimo commit

---
+++ models/devuelvoTweets.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type DevuelvoTweets struct {
+	ID      primitive.ObjectID `bson:"id" json:"id,omitempty"`
+	UserId  string             `bson:"userid " json:"userid"`
+	Mensaje string             `bson:"mensaje "json:"mensaje"`
+	Fecha   time.Time          `bson:"fecha" json:"fecha"`
+}
---
+++ routers/leoTweets.go
@@ -0,0 +1,52 @@
+package routers
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/ngonzalezo/twitterGo/bd"
+	"github.com/ngonzalezo/twitterGo/models"
+	"strconv"
+)
+
+func LeoTweets(request events.APIGatewayProxyRequest) models.RespApi {
+	var r models.RespApi
+	r.Status = 400
+
+	ID := request.QueryStringParameters["id"]
+	pagina := request.QueryStringParameters["pagina"]
+
+	if len(ID) < 1 {
+		r.Message = "El parametro ID es obligatorio"
+		return r
+	}
+
+	if len(pagina) < 1 {
+		pagina = strconv.Itoa(1)
+	}
+
+	pag, err := strconv.Atoi(pagina)
+
+	if err != nil {
+		r.Message = "Debe enviar el parametro Pagina como un valor mayor a 0"
+		return r
+	}
+
+	tweets, correcto := bd.LeoTweets(ID, int64(pag))
+
+	if !correcto {
+		r.Message = "Error al leer los tweets"
+		return r
+	}
+
+	respJson, err := json.Marshal(tweets)
+
+	if err != nil {
+		r.Status = 500
+		r.Message = "Error al formatear los datos de los usuarios como JSON"
+		return r
+	}
+
+	r.Status = 200
+	r.Message = string(respJson)
+	return r
+}
