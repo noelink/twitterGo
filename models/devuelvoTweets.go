907cd5aa71c3559fe8aecf68a2483d8a8340bf42
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2024-07-02 16:49:36 -0600

    Ultimo commit

--- models/devuelvoTweets.go
+++ models/devuelvoTweets.go
@@ -8,6 +8,6 @@ import (
 type DevuelvoTweets struct {
 	ID      primitive.ObjectID `bson:"id" json:"id,omitempty"`
 	UserId  string             `bson:"userid " json:"userid"`
-	Mensaje string             `bson:"mensaje "json:"mensaje"`
+	Mensaje string             `bson:"mensaje " json:"mensaje"`
 	Fecha   time.Time          `bson:"fecha" json:"fecha"`
 }
--- routers/leoTweets.go
+++ routers/leoTweets.go
@@ -21,7 +21,7 @@ func LeoTweets(request events.APIGatewayProxyRequest) models.RespApi {
 	}
 
 	if len(pagina) < 1 {
-		pagina = strconv.Itoa(1)
+		pagina = "1"
 	}
 
 	pag, err := strconv.Atoi(pagina)
